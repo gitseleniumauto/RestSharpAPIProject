0ea5a64cb47c9be43d7dc0cf28669a95ef95c4f2
gitseleniumauto <39797222+gitseleniumauto@users.noreply.github.com>
2020-02-25 09:37:17 +1100

    Add files via upload
    Test commit

---
+++ RestsharpProject/Model/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestsharpProject.Model
+{
+    public class Post
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        
+    }
+}
+
